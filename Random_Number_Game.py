3f0f674c7e3a3aaf578754c605d8d9a512718252
AAA <aaa@aaa.com>
2017-08-28 16:54:19 -0500

    First commit

---
+++ Random_Number_Game.py
@@ -0,0 +1,14 @@
+import random
+x=random.randint(1, 10)
+loop=True
+
+while(loop==True):
+	guess=input("Guess integer between 1 and 10: ")
+	if int(guess) == x:
+		print("Correct! it was "+str(x))
+		loop=False
+	elif int(guess) > x:
+		print("Incorrect, Your guess is too high. Try again...")
+	else:
+		print("Incorrect, Your guess is too low. Try again.")
+	
