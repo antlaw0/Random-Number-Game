0d9682b864adfb1e0f37a58a1a1722db4e08cd0c
heather <crystal_tree@hotmail.com>
2017-08-31 18:56:02 -0500

    seeking conflict2

--- Random_Number_Game.py
+++ Random_Number_Game.py
@@ -1,13 +1,13 @@
 import random
-xx=random.randint(1, 10)
+xxx=random.randint(1, 10)
 loopy=True
 
 while(loopy==True):
 	guess=input("Guess integer between 1 and 10: ")
-	if int(guess) == xx:
-		print("Correct! it was "+str(xx))
+	if int(guess) == xxx:
+		print("Correct! it was "+str(xxx))
 		loopy=False
-	elif int(guess) > xx:
+	elif int(guess) > xxx:
 		print("Incorrect, Your guess is too high. Try again...")
 	else:
 		print("Incorrect, Your guess is too low. Try again.")
